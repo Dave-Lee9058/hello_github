4fa998864a3f7398e8d5f7e6e995a11c20fff3a8
Dosoon Lee <dosoon.lee@hp.com>
2019-11-18 00:16:53 -0800

    Init Repo and Add code

---
+++ HelloGithub.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file 5.10_cin_extraction.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-11-17
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ * @note
+ * 1. A program that handles erro cases well is said to be robust.
+ * 2. >> extraction ex> std::cin >> myValue.
+ * 
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello Github!!" << '\n';
+    return 0;
+}
